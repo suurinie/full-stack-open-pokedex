8f3aec2cd246cb3b575614b4168635b295f1d889
Saku Suuriniemi <saku.suuriniemi@iki.fi>
2021-02-10 11:38:19 +0200

    Test suppression of ESlint console error.

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+/* eslint no-console: ["error", { allow: ["warn", "error", "log"] }] */
+
 app.listen(PORT, () => {
   console.log('server started on port 5000')
 })
